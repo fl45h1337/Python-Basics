3c393bd4996a6c94110db6ea4efed986dcbda5ca
Harshiv Pandya <pandyaharshiv@gmail.com>
2017-12-01 09:15:49 +0530

    .

--- Python_Misc/Demos.py
+++ Python_Misc/Demos.py
@@ -19,21 +19,54 @@
 # from pprint import pprint
 # pprint([[0 for x in range(10)] for y in range(10)])
 
+#
+# import string
+#
+# for letter in string.ascii_lowercase:
+#     print(list(letter))
+#
+#     # Python code to demonstrate working of
+#     # center(), ljust() and rjust()
+#     str = "geeksforgeeks"
+#
+#     # Printing the string after centering with '-'
+#     print("The string after centering with '-' is : ", end="")
+#     print(str.center(20, '-'))
+#
+#     # Printing the string after ljust()
+#     print("The string after ljust is : ", end="")
+#     print(str.ljust(20, '-'))
+#
+#     # Printing the string after rjust()
+#     print("The string after rjust is : ", end="")
+#     print(str.rjust(20, '-'))
 
-    # Python code to demonstrate working of
-    # center(), ljust() and rjust()
+# line = "Geek1 Geek2 Geek3";
+# print(line.split(' ', 1))
+#
+# str1 = ''
+# str2 = 'geeks'
+#
+# # repr is used to print the string along with the quotes
+# print(repr(str1 and str2))  # Returns str1
+# print(repr(str2 and str1))  # Returns str1
+# print(repr(str1 or str2)) # Returns str2
+# print(repr(str2 or str1)) # Returns str2
+# print(repr(str2 or str1)) # Returns str2
 
+# letters  = []
+# check = "p y t h o n"
+# for letter in check:
+#         letters = check.split(" ")
+#         if letter in letter:
+#             letters.append(letter)
+# # print(letters)
+#
+# for letter in "Hello":
+#     if letter == "e":
+#       print(letter)
 
-    str = "geeksforgeeks"
+a = set([2,5])
+b = set([2,4,6])
 
-    # Printing the string after centering with '-'
-    print("The string after centering with '-' is : ", end="")
-    print(str.center(20, '-'))
-
-    # Printing the string after ljust()
-    print("The string after ljust is : ", end="")
-    print(str.ljust(20, '-'))
-
-    # Printing the string after rjust()
-    print("The string after rjust is : ", end="")
-    print(str.rjust(20, '-'))
+print(a<b)
